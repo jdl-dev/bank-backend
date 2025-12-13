4395fb93d1827488d33d292b6ec2cf2032e77f25
Daniel J <januszkiewiczdl@gmail.com>
2025-12-13 21:35:54 +0100

    SecurityConfig created, basic settings.

---
+++ src/main/java/com/app/bankbackend/common/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.app.bankbackend.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // API + JWT: na start wyłączamy
+                .sessionManagement(
+                        sessionManagement ->
+                                sessionManagement
+                                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/actuator/health",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults()) // może zostać na razie (do testów)
+                .build();
+    }
+}
