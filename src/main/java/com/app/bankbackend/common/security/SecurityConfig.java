6a5af368bec61eb88e2b3c6647bb185a671dcf8e
Daniel J <januszkiewiczdl@gmail.com>
2025-12-13 21:48:19 +0100

    PasswordEncoder added to SecurityConfig class.

--- src/main/java/com/app/bankbackend/common/security/SecurityConfig.java
+++ src/main/java/com/app/bankbackend/common/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -33,5 +35,10 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults()) // może zostać na razie (do testów)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
