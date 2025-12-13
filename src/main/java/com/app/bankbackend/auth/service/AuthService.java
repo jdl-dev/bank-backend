d154630e033f1f8a59cc13763548bff21310feda
Daniel J <januszkiewiczdl@gmail.com>
2025-12-13 22:22:50 +0100

    AuthService second method (login) created.

--- src/main/java/com/app/bankbackend/auth/service/AuthService.java
+++ src/main/java/com/app/bankbackend/auth/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.app.bankbackend.auth.service;
 
+import com.app.bankbackend.auth.api.dto.AuthResponse;
+import com.app.bankbackend.auth.api.dto.LoginRequest;
 import com.app.bankbackend.auth.api.dto.RegisterRequest;
 
 public interface AuthService {
     void register(RegisterRequest request) ;
+    AuthResponse login(LoginRequest request);
 }
--- src/main/java/com/app/bankbackend/auth/service/AuthServiceImpl.java
+++ src/main/java/com/app/bankbackend/auth/service/AuthServiceImpl.java
@@ -1,7 +1,10 @@
 package com.app.bankbackend.auth.service;
 
+import com.app.bankbackend.auth.api.dto.AuthResponse;
+import com.app.bankbackend.auth.api.dto.LoginRequest;
 import com.app.bankbackend.auth.api.dto.RegisterRequest;
 import com.app.bankbackend.auth.exception.authexception.EmailAlreadyUsedException;
+import com.app.bankbackend.auth.exception.authexception.InvalidCredencialsException;
 import com.app.bankbackend.domain.User;
 import com.app.bankbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -36,5 +39,32 @@ public class AuthServiceImpl implements AuthService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        String email = request
+                .email()
+                .trim()
+                .toLowerCase();
+
+        var user = userRepository
+                .findByEmailIgnoreCase(email)
+                .orElseThrow(InvalidCredencialsException::new);
+
+        if(!user.isEnabled()){
+            throw new InvalidCredencialsException();
+        }
+        if(user.isLocked()){
+            throw new InvalidCredencialsException();
+        }
+
+        boolean isPasswordGood =  passwordEncoder.matches(request.password(), user.getPasswordHash());
+
+        if(!isPasswordGood){
+            throw new InvalidCredencialsException();
+        }
+
+        return new AuthResponse("dummy-access-token", "dummy-refresh-token", 600);
     }
 }
